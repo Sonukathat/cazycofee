bae82f597b2b9b9d32f11eba678b8cf1f116c47b
sonukathat <skkhankathat93@gmail.com>
2025-12-02 12:30:51 +0530

    add items

---
+++ app/api/items/routes.js
@@ -0,0 +1,39 @@
+import { connectDB } from "@/lib/db";
+import Category from "@/lib/models/Category";
+import Item from "@/lib/models/Item";
+
+export async function POST(req) {
+  await connectDB();
+
+  const { categoryName, name, description, price, image } = await req.json();
+
+  if (!categoryName || !name || !price) {
+    return Response.json(
+      { error: "categoryName, name and price required" },
+      { status: 400 }
+    );
+  }
+
+  const category = await Category.findOne({ name: categoryName });
+
+  if (!category) {
+    return Response.json(
+      { error: "Category not found" },
+      { status: 404 }
+    );
+  }
+
+  const item = await Item.create({
+    categoryId: category._id,
+    name,
+    description,
+    price,
+    image,
+  });
+
+  return Response.json({
+    message: "Item Added",
+    item,
+    category: category.name,
+  });
+}
